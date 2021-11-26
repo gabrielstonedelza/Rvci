0ac8b18f70320093bd2981f2d6cec92c10054947
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-11-26 02:59:12 +0000

    added render to users.views import

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render,get_object_or_404
 from .models import User, Profile
 from .serializers import UsersSerializer, ProfileSerializer
 from rest_framework.decorators import api_view, permission_classes
