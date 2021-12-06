9bd02e296b1314c6324a3d40092f64c67e6f1564
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-12-06 10:12:17 +0000

    added fetching user specific profile pic

--- users/models.py
+++ users/models.py
@@ -30,15 +30,15 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.user_profile_pic.path)
+        img = Image.open(self.profile_pic.path)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.user_profile_pic.path)
+            img.save(self.profile_pic.path)
 
     def user_profile_pic(self):
-        if self.user_profile_pic:
-            return "http://127.0.0.1:8000" + self.user_profile_pic.url
+        if self.profile_pic:
+            return "http://127.0.0.1:8000" + self.profile_pic.url
 
         return ''
