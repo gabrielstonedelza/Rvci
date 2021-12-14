f3d43267e3a9df11c6d77925881af517fe9296fc
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-12-14 15:50:09 +0000

    added phone to profile serializer

--- users/serializers.py
+++ users/serializers.py
@@ -22,7 +22,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ['id', 'username', 'full_name', 'user', 'profile_pic','bio','user_profile_pic']
+        fields = ['id', 'username', 'full_name', 'user','phone', 'profile_pic','bio','user_profile_pic']
         read_only_fields = ['user']
 
     def get_username(self, user):
