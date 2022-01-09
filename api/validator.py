ab8b662459f93d03cf6eced0f545cf7e7b4dbfe5
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-01-09 15:53:39 +0000

    file size for stories updated

--- api/validator.py
+++ api/validator.py
@@ -3,16 +3,8 @@ from django.core.exceptions import ValidationError
 def validate_story_size(value):
     filesize = value.size
 
-    if filesize > 5242880:
+    if filesize > 20971520:
         raise ValidationError("File size cannot be above 5MB")
     else:
         return value
 
-
-def validate_devotion_size(value):
-    filesize = value.size
-
-    if filesize > 5242880:
-        raise ValidationError("File size cannot be above 5MB")
-    else:
-        return value
